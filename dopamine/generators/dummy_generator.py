67402d12d827f4dc99030b19617a759da91fe9de
Kacper Kielak <kielak.kacper@gmail.com>
2019-07-29 16:02:01 +0100

    Add dummy generator for testing/debugging GAIRL

---
+++ dopamine/generators/dummy_generator.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+# Copyright 2018 Kacper Kielak.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Dummy generator used for test purposes"""
+
+
+
+from dopamine.abstract_ml_model import AbstractMLModel
+import tensorflow as tf
+
+
+class DummyGenerator(AbstractMLModel):
+  """An implementation of the dummy generator"""
+
+  def __init__(self):
+    """Initializes the generator"""
+    tf.logging.info('Creating %s', self.__class__.__name__)
+
+  def bundle_and_checkpoint(self, checkpoint_dir, iteration_number):
+    """Pretends to checkpoint the generator.
+
+    Args:
+      checkpoint_dir: str, directory where TensorFlow objects would be saved.
+      iteration_number: int, iteration number that would be used for naming
+      the checkpoint file.
+
+    Returns:
+      An empty dict. If the checkpoint directory does not exist, returns None.
+    """
+    if not tf.gfile.Exists(checkpoint_dir):
+      return None
+
+    return {}
+
+  def unbundle(self, checkpoint_dir, iteration_number, bundle_dictionary):
+    """Pretends to restore the generator from a checkpoint.
+
+    Args:
+      checkpoint_dir: str, path to the checkpoint.
+      iteration_number: int, checkpoint version.
+      bundle_dictionary: dict, containing additional Python objects owned by
+      the agent.
+
+    Returns:
+      bool, True (dummy unbundling is always successful).
+    """
+    return True
